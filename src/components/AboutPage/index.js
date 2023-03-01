30e556b60cc7cc80e5233625f3fb2d62796b8204
Pierre Vanoverschelde <pierre.vano@gmail.com>
2023-03-01 15:20:21 +0100

    Update about section

--- src/components/AboutPage/index.js
+++ src/components/AboutPage/index.js
@@ -38,9 +38,11 @@ const AboutPage = () => {
             <Text xs={0} sm={1} style={{ margin: "1em 0" }}>
               This app purpose is to help you get quickly highest rated movies/tv shows or explore the information provided by <Anchor to="https://www.themoviedb.org">The Movie Database (TMDB)</Anchor>.
             </Text>
-
             <Text style={{ margin: "1em 0" }}>
-              I have added some new features (ratings filters and theaters list) but most of the codebase comes from this <Anchor to="https://github.com/vitordino/movies">repository</Anchor> and all the design credits should be addressed to <Anchor to="https://vitordino.com">Vitor Dino</Anchor>.
+              I have added some new features (ratings filters and cinemas search) but most of the codebase comes from this <Anchor to="https://github.com/vitordino/movies">repository</Anchor> and all the design credits should be addressed to <Anchor to="https://vitordino.com">Vitor Dino</Anchor>.
+            </Text>
+            <Text style={{ margin: "1em 0" }}>
+              The raw data comes from AlloCiné, IMDb, BetaSeries and The Movie Database, all extracted with shell and Node.js scripts and pushed to a MongoDB. Then on the client side it's built with ReactJS and fetched from an Express.js API. Feel free to visit my <Anchor to="https://github.com/pierrevano">GitHub</Anchor> for more details.
             </Text>
           </Cell>
         </Row>
