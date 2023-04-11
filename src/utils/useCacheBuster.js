3aadd70aa03fee7a23463b68496045d46d21c4ff
Pierre Vanoverschelde <pierre.vano@gmail.com>
2023-04-11 21:11:04 +0200

    Update useCacheBuster

--- src/utils/useCacheBuster.js
+++ src/utils/useCacheBuster.js
@@ -1,6 +1,27 @@
 import { useEffect } from "react";
 import { useStorageString } from "./useStorageString";
 
+/**
+ * Refreshes the cache and reloads the current page.
+ * @returns None
+ */
+const refreshCacheAndReload = () => {
+  if (caches) {
+    // Service worker cache should be cleared with caches.delete()
+    caches.keys().then((names) => {
+      for (const name of names) {
+        caches.delete(name);
+      }
+    });
+  }
+  // delete browser cache and hard reload
+  window.location.reload(true);
+};
+
+/**
+ * A custom React hook that checks for updates to the app version and refreshes the cache if necessary.
+ * @returns {null}
+ */
 const useCacheBuster = () => {
   const parseVersion = (str) => +str.replace(/\D/g, "");
   const [version, setVersion] = useStorageString("version", "1.0.0");
@@ -15,7 +36,7 @@ const useCacheBuster = () => {
           if (parseVersion(version) < parseVersion(metaVersion)) {
             if (window?.location?.reload) {
               setVersion(metaVersion);
-              window.location.reload(true);
+              refreshCacheAndReload();
             }
           }
         }
