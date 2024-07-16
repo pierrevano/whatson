7af7c8edd52e51bcc33e3ac5e7dd815f30e499c2
Pierre Vanoverschelde <pierre.vano@gmail.com>
2024-07-16 16:29:27 +0200

    fix: update cache buster

--- src/utils/useCacheBuster.js
+++ src/utils/useCacheBuster.js
@@ -5,11 +5,15 @@ const majorVersion = "2.5.0";
 
 /**
  * Refreshes the cache and reloads the current page.
+ * @returns None
  */
-const refreshCacheAndReload = async () => {
+const refreshCacheAndReload = () => {
   if (caches) {
-    const cacheNames = await caches.keys();
-    await Promise.all(cacheNames.map((name) => caches.delete(name)));
+    caches.keys().then((names) => {
+      for (const name of names) {
+        caches.delete(name);
+      }
+    });
   }
   window.location.reload(true);
 };
@@ -18,17 +22,15 @@ const refreshCacheAndReload = async () => {
  * A custom React hook that fetches a meta.json file and compares the version number
  * to the current package version. If the meta.json version is greater than the package
  * version, the page is reloaded to update the cache.
+ * @returns {null}
  */
 const useCacheBuster = () => {
-  useEffect(() => {
-    const parseVersion = (str) => +str.replace(/\D/g, "");
+  const parseVersion = (str) => +str.replace(/\D/g, "");
 
-    const fetchMetaAndUpdateCache = async () => {
-      try {
-        const response = await fetch(`/meta.json?v=${+new Date()}`, {
-          cache: "no-cache",
-        });
-        const meta = await response.json();
+  useEffect(() => {
+    fetch(`/meta.json?v=${+new Date()}`, { cache: "no-cache" })
+      .then((response) => response.json())
+      .then((meta) => {
         if (meta?.version) {
           const metaVersion = parseVersion(meta.version);
           const packageVersion = parseVersion(version);
@@ -40,16 +42,16 @@ const useCacheBuster = () => {
             localStorage.clear();
           }
 
-          if (packageVersion < metaVersion && window?.location?.reload) {
-            refreshCacheAndReload();
+          if (packageVersion < metaVersion) {
+            if (window?.location?.reload) {
+              refreshCacheAndReload();
+            }
           }
         }
-      } catch (error) {
-        console.error("Error fetching meta.json:", error);
-      }
-    };
-
-    fetchMetaAndUpdateCache();
+      })
+      .catch((error) => {
+        console.error("something went wrong fetching meta.json", error);
+      });
   }, []);
 
   return null;
